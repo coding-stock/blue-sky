00c442cabcc362909879dd3207e35e404fb026b0
coding-stock <alimaboyi@gmail.com>
2025-08-24 08:30:06 +0200

    fk yeah

---
+++ js.js
@@ -0,0 +1,21 @@
+// const collide_box = document.querySelector('.collide');
+// const h1 = document.querySelector('.skills_text h1');
+// const h3 = document.querySelector('.skills_text h3');
+
+// let boom = false;
+
+// window.addEventListener('scroll', () => {
+//   const collide_box_rect = collide_box.getBoundingClientRect();
+//   const h1_rect = h1.getBoundingClientRect();
+
+//   if (!boom && h1_rect.top <= collide_box_rect.bottom) {
+//     boom = true;
+//     console.log("💥 h1 touched collide bottom");
+//     h3.style.display = "none";
+//   } 
+//   else if (boom && h1_rect.top > collide_box_rect.bottom) {
+//     boom = false;
+//     console.log("⬆️ h1 moved back above collide bottom");
+//      h3.style.display = "block";
+//   }
+// });
